5f6d124a9205a37fabc6f99d3d20a7c2ad4c19c0
Adam Rice <adamdanielrice1@gmail.com>
2020-06-15 17:03:09 -0500

    Halfway through exercise 1

--- oop1-construct.js
+++ oop1-construct.js
@@ -1,42 +1,104 @@
 //1. Create a Class called WhyClass.  This class should have one property, and two methods.  THe property is called "purpose", and stores a string.  This string should clearly explain why classes are used in Object Oriented Programming.  The first method is called explain().  This method should print the contents stored in the purpose 'field' of the class.  The second method is called pieces().  This 2nd method should print out the various parts that are essential to building a class.  Instantiate the class, and invoke the method so the results are showing in the console once this file is run.
 
-//your code here...
-
-
+class WhyClass {
+	constructor() {}
+	property =
+		'Class is a good feature of Javascript. Classes allow you to design your code to be reusable and to function like real life things. Nouns and verbs.';
+	explain() {
+		console.log(this.property);
+	}
+	pieces() {
+		console.log('constructor function, new keyword, . notation, methods.');
+	}
+}
 
 /*2. You are exploring the rainforests of the Amazon.  You have observed many different types of wildlife on your excursion.  To jog your memory, some different types of wildlife that you observed are found in the following URL : https://www.ietravel.com/blog/amazon-rainforest-animals-beginners-guide-21-species.  Feel free to use other resources online if you wish.
 
 Create a class that can categorize at least 6 of the creatures that you recall observing the Amazon.  Each creature should have 3 unique characteristics, and 2 unique actions that it can do. */
 
-
 //your code here...
-
-
-
+class Animal {
+	constructor(name, size, color) {
+		(this.name = name), (this.size = size), (this.color = color);
+	}
+	speak() {
+		console.log('I am a' + this.name);
+	}
+	describe() {
+		console.log('I am' + this.color, 'and', +this.size);
+	}
+}
 
 /*3. Convert the following Object Literals into a general class called Shape, and instantiate the class to generate the object literals
-*/
-
+ */
 
 class Shape {
-    //your code here...
-}
-
-
-const triangle = {
-    name : "triangle",
-    sides : [4,7,7], //lengths of each side
-    base : null,
-    height : null, //determine this height using basic geometry.  You may need to research this one.
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${.5 * this.base * this.height}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(this.base) + (this.sides[1] + this.sides[2]) }`)
+	constructor(name, sides, base, height, radius) {
+		(this.name = name),
+			(this.sides = sides),
+			(this.base = base),
+			(this.height = height);
+	}
+	calcArea() {
+		if ((this.sides.length = 3)) {
+			let area = (this.base * this.height) / 2;
+			console.log(`${this.name}'s area is ${area}`);
+		}
+		if ((this.sides.length = 4)) {
+			let area = this.base * this.height;
+			console.log(`${this.name}'s area is ${area}`);
+		}
+		if ((this.sides.length = 1)) {
+			let area = (Math.PI * this.radius ** 2).toFixed(2);
+			console.log(`${this.name}'s area is ${area}`);
+		}
+	}
+	calcHeight() {
+		if ((this.sides = 3)) {
+			this.height = Math.sqrt((this.sides[0]**2) + (this.sides[0]**2))
+		}}
+	calcPerimeter() {
+		if ((this.sides.length = 1)) {
+			let perimeter = (2 * Math.PI * this.radius).toFixed(2);
+			console.log(
+				`This is the perimeter of the ${this.name}: ${perimeter}`
+			);
+		} else {
+			let perimeter = 0;
+			for (var i = 0; i < this.sides.length; i++) {
+				perimeter = perimeter + sides[i];
+			}
+			console.log(
+				`This is the perimeter of the ${this.name}: ${perimeter}`
+			);
+		}
     }
 }
+rect = new Shape('rectangle', [2,5,5,2], 2,5, null)
+rect = new Shape('triangle', [4,7,7] 4,7, null)
+rect = new Shape('rectangle', 1, null, null, 5)
 
-triangle.base = triangle.sides[0];  //base is the 1st side in the triangle.sides array.
+const triangle = {
+	name: 'triangle',
+	sides: [4, 7, 7], //lengths of each side
+	base: null,
+	height: null, //determine this height using basic geometry.  You may need to research this one.
+	calcArea: function() {
+		console.log(
+			`${this.name}'s area is calculated to be : ${0.5 *
+				this.base *
+				this.height}`
+		);
+	},
+	calcPerimeter: function() {
+		console.log(
+			`${this.name}'s perimeter is calculated to be : ${this.base +
+				(this.sides[1] + this.sides[2])}`
+		);
+	}
+};
+
+triangle.base = triangle.sides[0]; //base is the 1st side in the triangle.sides array.
 triangle.height = 6.71; //determine the height using basic geometry.  How do you calculate the height of a triangle with 2 equal sides?  If you use the formula to caluclate the height, this is a bonus.  If you hard code the correct value here, that will be sufficient, but no bonus :(
 
 console.log(triangle);
@@ -44,38 +106,54 @@ triangle.calcArea();
 triangle.calcPerimeter();
 
 const rectangle = {
-    name : "rectangle",
-    sides : 4,
-    length : 2,
-    width : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${this.length * this.width}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(2 * this.length) + (2 * this.width) }`)
-    }
-}
+	name: 'rectangle',
+	sides: 4,
+	length: 2,
+	width: 5,
+	calcArea: function() {
+		console.log(
+			`${this.name}'s area is calculated to be : ${this.length *
+				this.width}`
+		);
+	},
+	calcPerimeter: function() {
+		console.log(
+			`${this.name}'s perimeter is calculated to be : ${2 * this.length +
+				2 * this.width}`
+		);
+	}
+};
 
 console.log(rectangle);
 rectangle.calcArea();
 rectangle.calcPerimeter();
 
 const circle = {
-    name : "circle",
-    sides : 1,
-    radius : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${(Math.PI * this.radius**2).toFixed(2)}`);
-    },
-    calcCircumference : function() {
-        console.log(`${this.name}'s circumference is calculated to be : ${(2 * Math.PI * this.radius).toFixed(2)}`)
-    }
-}
+	name: 'circle',
+	sides: 1,
+	radius: 5,
+	calcArea: function() {
+		console.log(
+			`${this.name}'s area is calculated to be : ${(
+				Math.PI *
+				this.radius ** 2
+			).toFixed(2)}`
+		);
+	},
+	calcCircumference: function() {
+		console.log(
+			`${this.name}'s circumference is calculated to be : ${(
+				2 *
+				Math.PI *
+				this.radius
+			).toFixed(2)}`
+		);
+	}
+};
 
 console.log(circle);
 circle.calcCircumference();
 circle.calcArea();
-
 
 /*********************************************** 
 Bonus Exercises:
@@ -88,19 +166,14 @@ Bonus Exercises:
 */
 
 class Earth {
-    name;
-    planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
+	name;
+	planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
 
-    constructor(name,num) {
-        this.name = name;
-        this.planetNum = num;
-    }
-
+	constructor(name, num) {
+		this.name = name;
+		this.planetNum = num;
+	}
 }
 
-const earth = new Earth('earth',3);
+const earth = new Earth('earth', 3);
 console.log(earth);
-
-
-
-
